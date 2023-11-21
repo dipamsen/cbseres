ffee86335bc3b084106e12e71a6b7d59f09c6f1b
Dipam Sen <dipamdiptam@gmail.com>
2023-11-21 22:37:53 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,221 @@
+import { useState } from "react";
+import { data } from "./data.js";
+
+function App() {
+  const [subject, setSubject] = useState("phy");
+
+  const parsed = (input) => {
+    let obj = {};
+    if (input.curr) {
+      obj["Curriculum (LO + Syllabus + Pattern)"] = input.curr;
+    }
+    if (input.sqp && input.ms) {
+      obj["Sample Question Paper"] = [input.sqp, input.ms];
+    }
+    if (input.pq && input.pqms) {
+      obj["Additional Practice Questions"] = [input.pq, input.pqms];
+    }
+    if (input.pq2 && input.pq2ms) {
+      obj["Additional Practice Questions 2"] = [input.pq2, input.pq2ms];
+    }
+    if (input.qb) {
+      obj["Question Bank"] = input.qb;
+    }
+    return obj;
+  };
+
+  const thisYr = parsed(data[subject].thisyear);
+  const pastYr = parsed(data[subject].prevyear);
+
+  const pyqs = data[subject].pyqs;
+
+  return (
+    <div>
+      {/* <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            CBSE Resources
+          </a>
+        </div>
+      </nav> */}
+      <div style={{ height: "50px" }}></div>
+      <div className="container-sm" style={{ maxWidth: "700px" }}>
+        <h1>CBSE Resources</h1>
+        <label className="form-label">
+          Subject:
+          <select
+            value={subject}
+            className="form-select"
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+          >
+            <option value="phy">Physics - 042</option>
+            <option value="che">Chemistry - 043</option>
+            <option value="mat">Mathematics - 041</option>
+            <option value="eng">English (Core) - 301</option>
+            <option value="cse">Computer Science - 083</option>
+          </select>
+        </label>
+
+        <div className="list-group">
+          <div className="list-group-item list-group-item-primary">
+            <h5 className="text-light strong m-0">Current Year</h5>
+          </div>
+          {Object.entries(thisYr).map(([item, link], i) => (
+            <div
+              className="list-group-item"
+              key={i}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div>{item} 2023-24</div>
+              <div className="btn-group">
+                {typeof link == "string" ? (
+                  <a
+                    href={link}
+                    target="_blank"
+                    rel="noreferrer"
+                    key={i}
+                    className="btn btn-primary"
+                  >
+                    Open
+                  </a>
+                ) : (
+                  typeof link == "object" &&
+                  Array.isArray(link) && (
+                    <>
+                      <a
+                        className="btn btn-primary"
+                        href={link[0]}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={i}
+                      >
+                        Question Paper
+                      </a>
+                      <a
+                        className="btn btn-primary"
+                        href={link[1]}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={i}
+                      >
+                        Answers
+                      </a>
+                    </>
+                  )
+                )}
+              </div>
+            </div>
+          ))}
+          <div
+            className="list-group-item list-group-item-primary"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <h5 className="text-light strong m-0">PYQs (2023)</h5>
+            <div>(same syllabus as 2024)</div>
+          </div>
+          {pyqs.map((item, i) => (
+            <div
+              className="list-group-item"
+              key={i}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div>Set {i + 1}</div>
+              <div className="btn-group">
+                <a
+                  href={item.qp}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-primary"
+                >
+                  Question Paper
+                </a>
+                <a
+                  href={item.ms}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-primary"
+                >
+                  Answers
+                </a>
+              </div>
+            </div>
+          ))}
+          <div className="list-group-item list-group-item-primary">
+            <h5 className="text-light strong m-0">Previous Year(s)</h5>
+          </div>
+          {Object.entries(pastYr).map(([item, link], i) => (
+            <div
+              className="list-group-item"
+              key={i}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div>
+                {item == "Question Bank"
+                  ? "CBSE Case Based Question Bank"
+                  : `${item} 2022-23`}
+              </div>
+              <div className="btn-group">
+                {typeof link == "string" ? (
+                  <a
+                    href={link}
+                    target="_blank"
+                    rel="noreferrer"
+                    key={i}
+                    className="btn btn-primary"
+                  >
+                    Open
+                  </a>
+                ) : (
+                  typeof link == "object" &&
+                  Array.isArray(link) && (
+                    <>
+                      <a
+                        className="btn btn-primary"
+                        href={link[0]}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={i}
+                      >
+                        Question Paper
+                      </a>
+                      <a
+                        className="btn btn-primary"
+                        href={link[1]}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={i}
+                      >
+                        Answers
+                      </a>
+                    </>
+                  )
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div style={{ height: "100px" }}></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,185 @@
+// English (Core) - 301
+// Computer Science - 083
+// Mathematics - 041
+// Physics - 042
+// Chemistry - 043
+
+export const data = {
+  phy: {
+    thisyear: {
+      curr: "https://drive.google.com/file/d/136xzY3DhdwgLOzCLE9eyOGo64XfZIHIw/view?usp=drive_link",
+      sqp: "https://drive.google.com/file/d/1ri9wsrZ5V_0PtaLz883ofXnhxNx8Fnku/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1vRBhxuFSXVv6rAxmB9gzpzoxaPnzimW-/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/1DAyTrxqmP1WGsoUZN6vKyvVN0Ug0GxVO/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/16TTKs_03SsyOjkdP7mZ8ygnQi64wb40a/view?usp=drive_link",
+    },
+    pyqs: [
+      {
+        qp: "https://drive.google.com/file/d/1eiIgi8OJY0MpsDfqhCsC9GhxTHtsT4hg/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1ccgMwo_bNczU4pYYQ-MsKRTSXwC-nI27/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1wVSt0yWHPRkK1jU4lKpVElXLlyvGrYoh/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1IqrQwSC8EY1QSLXIA-OxaRyvVGeY-N6z/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1u2sEkFEPhrBlsMozaGKY82zh7-h38qWz/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1vCDDA7Hl2pGY1yjjvzFyVoctSY4hHIQY/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/140PS1OvWpnAwhmBaxqBVq19cg5hqlEV3/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1GScShkINAzD8uYjY--i_Fx6mX_EPsFEo/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1LbCrEIfzfS__MV5VrGBjTZDKL2h_-p0O/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1vGYpls7sT0gAkXb7v4ixE7rZrQNbJNNB/view?usp=drive_link",
+      },
+    ],
+    prevyear: {
+      sqp: "https://drive.google.com/file/d/1emyPJTzLC7RtGkygb9q6hoxSBPjQQIvZ/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1Ntgcw2s1RUzZ6OY1NZqd2KMidl2J-pgf/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/1cqKOt3pbREZlMf_6yg6ykEX_IXdxXAKd/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/1Ntgcw2s1RUzZ6OY1NZqd2KMidl2J-pgf/view?usp=drive_link",
+    },
+  },
+  che: {
+    thisyear: {
+      curr: "https://drive.google.com/file/d/1KUYHfEXml3qkUIUrnIln3enOPyyT1Kb0/view?usp=drive_link",
+      sqp: "https://drive.google.com/file/d/11Q5dpvECh1ttLNyngOZEgP_onguPw9Zl/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1Z8_BiuPPy7J-Dx4R0WMg1ZwDCQQ7mDfc/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/1f4eIQ_FIbcgCBpOTcZNH2TmOeGwPJx4R/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/1rxpVCczH6O2TbilfZR_AWSUpOfVbABcv/view?usp=drive_link",
+      pq2: "https://drive.google.com/file/d/1Bw1tKVUydZCT6O0JcDrkKivrGHfGFTm9/view?usp=drive_link",
+      pq2ms:
+        "https://drive.google.com/file/d/13fO4qpqGszqdYuuVHj4-LgQrwhQoqvtB/view?usp=drive_link",
+    },
+    pyqs: [
+      {
+        qp: "https://drive.google.com/file/d/18ah9tN1gQUJ7IuAXekwGUZtHXWv1UBez/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1_aUTR6g1vgdtcPdSuHT-6Uvg7L0YLnJI/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1btIq_TjSS21uxbMrTRBZxIrDfP7AIDDb/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1SCJcCI258T67RI8DkdwwXXaqiNPTG2lR/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/18gw4ZUragucjmxWj4PUSob8gVRkiaqhl/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1kDNmdLJfWUpdxCn8YpdgKTobo719ZGVJ/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1jaWibrm5K-tBARuzG3-HCOt6l8avdB4g/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1_U06hl2JPSIVqFd6IkJ4xNB79xZWYfq6/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1K_iuYxee2WWB3DCaz-ZBjvjpVja-L-0B/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1J3IJzjh5MQgEk8WBX8NT3Y_VSPi-q51P/view?usp=drive_link",
+      },
+    ],
+    prevyear: {
+      sqp: "https://drive.google.com/file/d/1-RcyXDtO8rfZIdEsLcqFJR0cEhRFO4MM/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1hTAKPR-6nIVK8WVMkibglLiHfWnV5RqD/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/1_5SDTG8g9xLblaPY3ofOtQ5-CXgDSLmI/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/1T2huqkLqOeBtwIfjtJFoW89ltFK1IBsg/view?usp=drive_link",
+      qb: "https://drive.google.com/file/d/1tTydNpcAD32cVj3CUrj_-5hK-aen4AVz/view?usp=drive_link",
+    },
+  },
+  cse: {
+    thisyear: {
+      curr: "https://drive.google.com/file/d/1P9-HjYjxQgZnCLEM8muJhPIlcylpNE8C/view?usp=drive_link",
+      sqp: "https://drive.google.com/file/d/1KUApf_oZp5-lnfQg4VhyEsB9w4jF-aij/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1rMpCV545oZqvzZbFJWKY7D313LDwKMYh/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/1Mc1LC-G-XSZk1EL0KflHRS97ZaBTPhF_/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/1PW6MpPE1PUlNsV3Q1MWTpkbLD6pJobrh/view?usp=drive_link",
+    },
+    pyqs: [
+      {
+        qp: "https://drive.google.com/file/d/1Wpbmz-ID5Qa4VdUKYakIi450aEsdcMQh/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1-w3T_Tlwp1ybjTbRPKLs4pNr0wjujuPf/view?usp=drive_link",
+      },
+    ],
+    prevyear: {
+      sqp: "https://drive.google.com/file/d/1WyQPWUGcugIWifNmnPgXLwqhbyUcov7M/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1Et35SEnv3WwYVv9q5AbvTh-i24We_IoI/view?usp=drive_link",
+      qb: "https://drive.google.com/file/d/1UlglRcuD6hV4sEgcm0ChjGSn-F_r2yVs/view?usp=drive_link",
+    },
+  },
+  eng: {
+    thisyear: {
+      curr: "https://drive.google.com/file/d/1warK57FB7lB1zf_xadjlSwxsfMBizyb2/view?usp=drive_link",
+      sqp: "https://drive.google.com/file/d/1_jSvHeY6gTZ0em_MmuI90XaPkXgNRMh2/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/16d8cylF5E2eX4Usm7gbwjHeBKCEf_Xxh/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/1IhXQy3ii2dWl2URv9xhZacXYAuHTl8P2/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/1y3RTfs0bOVPznk7izhHCP5OaoOAj8tHE/view?usp=drive_link",
+    },
+    pyqs: [
+      {
+        qp: "https://drive.google.com/file/d/1V301eZY7Uq1JpaIWloQlUIajwoYo-Sm1/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/17PGJdEleMOmnJyZSMU8hJFhCCWQ9QFPb/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1TIUzNoKljYBkAiMe2vvOjXpSXBx3Y9n4/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1fpwIUdEk94Uq3pcmn0sCaGedJK77FvgB/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1rfhOZQ5zDXr1glv4xZyjslRouvhE8kh3/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1Xxf43Cg_dVZfBRbGxhnJ07-OvDwrX8c-/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1hSp-3St_DGI0ioqX5xK4d7WhRNlNHC4i/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1VTC_ba3cSQ9z6FapDvXBrx62tMqIC2J7/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1j2-XSnzq5OVDDuPMrI68urNKFVrlKK4w/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1BN18DBPdx8qvqvFZ3kcH9u8Kbjgn-4m9/view?usp=drive_link",
+      },
+    ],
+    prevyear: {
+      sqp: "https://drive.google.com/file/d/10d05Z4mGPM09U9EQK1VDWrhVN9swpq5Y/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1_NMtNH1GMXe6ySyAD1vFuwqC2vM7V7s3/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/1FYdSfuKKL70T5xq8ei4eAvIsevV4RRN5/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/1QjYOKgUhkyygXUEkxETuV9NPIc91Cmxk/view?usp=drive_link",
+      qb: "https://drive.google.com/file/d/1tqb8U3bsbF-MWmiseU_xDj39jtRP4HpW/view?usp=drive_link",
+    },
+  },
+  mat: {
+    thisyear: {
+      curr: "https://drive.google.com/file/d/16LPjPGcQ0tmCAyK8vFiYeS99TcZsQNul/view?usp=drive_link",
+      sqp: "https://drive.google.com/file/d/1P4UlYNwfUVAZ1W4w7XcwdXkjSRWakk_k/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1Q0nXMVk2bQjrc0fr3TTwFLgxVt80QwTM/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/1V3QM3uNXGfFuSgdrZ7QXwujQcEtHmYdj/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/1f9PI7k9abwKEIsEvmmV6oZlQ-foXOGRC/view?usp=drive_link",
+      pq2: "https://drive.google.com/file/d/1cPBxmdPoJyRrie1oex00bl4RhV7SqJn6/view?usp=drive_link",
+      pq2ms:
+        "https://drive.google.com/file/d/1_pWs_tCsL-N6XE331mxHjxvWiwGLHadH/view?usp=drive_link",
+    },
+    pyqs: [
+      {
+        qp: "https://drive.google.com/file/d/11y8FmUdlHKQX8wiGlqpzdkORxhoREKqz/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1py9fOYBhD9rWyRv7PdZb-W7jksrymgjT/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/17N8-CUAF7H8bjEMdlIwosJiaJhUVvhOZ/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1YoRvk6QIgWzqz_1Ajsel1KVqx_sAcipC/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1lpB1P95wEf0ETALLmxceLk4UNHOlzDzY/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1gA0EjYIAzFeiODQste7Pm6oOXHPXl2CV/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1CJ8ZZJm70wzLqhuwIP2tKGgzuFb-Z4kw/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1BWsrOoi-BZARnOxbTzjzTykmaE8weovV/view?usp=drive_link",
+      },
+      {
+        qp: "https://drive.google.com/file/d/1DkYLVC2WiCxBdm2qOUqoMCOxLJe7AmhU/view?usp=drive_link",
+        ms: "https://drive.google.com/file/d/1HqASFrTBKG1U_TibiyWj5z2PEBFCwK4F/view?usp=drive_link",
+      },
+    ],
+    prevyear: {
+      sqp: "https://drive.google.com/file/d/1EUVmUWD4xgIdA2VVDH66szk1Rx1gf7Ci/view?usp=drive_link",
+      ms: "https://drive.google.com/file/d/1nledysZ5Js5m5BgS77DOAPkj922x23D9/view?usp=drive_link",
+      pq: "https://drive.google.com/file/d/14BYVS0_umxe-DY_oU-nNmiR4nmYFTFCA/view?usp=drive_link",
+      pqms: "https://drive.google.com/file/d/1TsvwsEiV5Ylu8ZZdAloEOVYzzsJbjXBD/view?usp=drive_link",
+      qb: "https://drive.google.com/file/d/1WG1yDCPm_th7lqyQ3f8JDYFz93njI1LX/view?usp=drive_link",
+    },
+  },
+};
